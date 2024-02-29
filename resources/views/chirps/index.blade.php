82fe60678067b66481c31b3c1e044bf4a956310a
MairanoTorena <torenabastos@gmail.com>
2024-02-29 11:37:36 -0300

    Added the sending of chirp forms

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -3,13 +3,22 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Chirps') }}
         </h2>
-    </x-slot>
+    </x-slot>                        
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Chirps form
+                    <form action="" method="POST">
+                        @csrf
+                        <textarea 
+                            name="message" 
+                            placeholder="{{ __('What\'s on your mind?') }}"
+                            class="dark:bg-slate-200 block w-full shadow-sm rounded-none mb-3 border-gray-300"
+                        >
+                        </textarea>
+                        <x-primary-button>{{ __('Chirp') }}</x-primary-button> 
+                    </form>
                 </div>
             </div>
         </div>
